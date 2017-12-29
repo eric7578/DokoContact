b0a291adb1c4345e1e9daa5de30d62d68795001c
ericyan <eric7578@gmail.com>
2017-12-30 00:12:39 +0800

    logout user if token is invalid

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const index = require('./routes/index')
-const oauth2callback = require('./routes/oauth2callback')
 
 const app = express()
 
@@ -29,7 +28,6 @@ app.use(cookieSession({
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
-app.use('/oauth2callback', oauth2callback)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,48 @@
 const express = require('express')
+const wrapper = require('./wrapper')
 const oauth = require('../services/oauth2')
 
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', redirectIfLogin, (req, res, next) => {
   res.render('index', {
     title: 'DokoContact',
     authUrl: oauth.getAuthUrl()
   })
 })
+
+router.get('/oauth2callback', redirectIfLogin, wrapper(async (req, res, next) => {
+  const tokens = await oauth.exchangeAccessToken(req.query.code)
+  req.session.accessToken = tokens.access_token
+  req.session.tokenType = tokens.token_type
+  req.session.expiryDate = tokens.expiry_date
+
+  res.redirect('/search')
+}))
+
+router.get('/search', redirectIfNotLogin, (req, res) => {
+  res.json(req.session)
+})
+
+function redirectIfLogin (req, res, next) {
+  const { accessToken, expiryDate } = req.session
+  if (accessToken && expiryDate > Date.now()) {
+    res.redirect('/search')
+  } else {
+    res.session = null
+    next()
+  }
+}
+
+function redirectIfNotLogin (req, res, next) {
+  const { accessToken, expiryDate } = req.session
+  if (accessToken && expiryDate > Date.now()) {
+    next()
+  } else {
+    res.session = null
+    res.redirect('/')
+  }
+}
 
 module.exports = router
